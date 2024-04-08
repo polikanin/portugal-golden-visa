6b8a78418c0aae017affa99139faf20bc77e1770
Anatolii Mykhailov <r.link.club@gmail.com>
2024-04-09 01:21:58 +0300

    Add new primary menu

--- wp-content/themes/proxima/inc/theme.inc.php
+++ wp-content/themes/proxima/inc/theme.inc.php
@@ -9,9 +9,11 @@ add_action( 'after_setup_theme', function () {
     add_theme_support( 'post-thumbnails' );
 
     register_nav_menus( [
-        'primary'     => 'Primary', // __( 'Primary', 'proxima' ),
-        'footer'      => 'Footer', // __( 'Footer', 'proxima' ),
-        'footer_tiny' => 'Footer Tiny', // __( 'Footer', 'proxima' ),
+        'primary'        => 'Primary', // __( 'Primary', 'proxima' ),
+        'primary_second' => 'Primary Second', // __( 'Primary', 'proxima' ),
+        'primary_mobile' => 'Primary Mobile', // __( 'Primary', 'proxima' ),
+        'footer'         => 'Footer', // __( 'Footer', 'proxima' ),
+        'footer_tiny'    => 'Footer Tiny', // __( 'Footer', 'proxima' ),
         // 'mobile'      => 'Mobile', // __( 'Mobile', 'marck' ),
         // 'lang'        => 'Language switcher', // __( 'Language switcher', 'proxima' ),
     ] );
