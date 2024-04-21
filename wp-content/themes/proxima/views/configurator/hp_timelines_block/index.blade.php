daa35dce38d1cefb6308c7750ca120d720fc8d02
apolikanin <apolikanin@webiprog.de>
2024-04-21 16:36:49 +0300

    fix timeline block

--- wp-content/themes/proxima/views/configurator/hp_timelines_block/index.blade.php
+++ wp-content/themes/proxima/views/configurator/hp_timelines_block/index.blade.php
@@ -12,56 +12,63 @@
                 {!! $text !!}
             </p>
         @endif
-
-        @if($timelines)
-            <div class="vp-table">
-                @foreach($timelines as $timeline)
-                    <div class="vp-table--body">
-                        <div class="vp-table--head">
-                            <div class="vp-table--col">
-                                <div class="vp-table--index">
-                                    @if($loop->index + 1 < 10)
-                                        0
-                                    @endif
-                                    {{ $loop->index + 1 }}
+    </div>
+    @if($timelines)
+        <div class="vp-table">
+            @foreach($timelines as $timeline)
+                <div class="vp-table--body">
+                    <div class="vp-table--head">
+                        <div class="wrapper">
+                            <div class="vp-table--head-inner">
+                                <div class="vp-table--col">
+                                    <div class="vp-table--index">
+                                        @if($loop->index + 1 < 10)
+                                            0
+                                        @endif
+                                        {{ $loop->index + 1 }}
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="vp-table--col">
-                                <div class="vp-table--heading">
-                                    {{ $timeline['name'] }}
+                                <div class="vp-table--col">
+                                    <div class="vp-table--heading">
+                                        {{ $timeline['name'] }}
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="vp-table--col">
+                                <div class="vp-table--col">
 
+                                </div>
                             </div>
                         </div>
-                        @if($timeline['steps'])
-                            @foreach($timeline['steps'] as $item)
-                                <div class="vp-table--row">
-                                    <div class="vp-table--col">
-                                        <div class="vp-table--title">
-                                            {{ $item['duration'] }}
+                    </div>
+                    @if($timeline['steps'])
+                        @foreach($timeline['steps'] as $item)
+                            <div class="vp-table--row">
+                                <div class="wrapper">
+                                    <div class="vp-table--row-inner">
+                                        <div class="vp-table--col">
+                                            <div class="vp-table--title">
+                                                {{ $item['duration'] }}
+                                            </div>
+                                            <div class="vp-table--quote">
+                                                {{ $item['quote'] }}
+                                            </div>
                                         </div>
-                                        <div class="vp-table--quote">
-                                            {{ $item['quote'] }}
+                                        <div class="vp-table--col">
+                                            <div class="vp-table--title">
+                                                {!! $item['title'] !!}
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="vp-table--col">
-                                        <div class="vp-table--title">
-                                            {!! $item['title'] !!}
-                                        </div>
-                                    </div>
-                                    <div class="vp-table--col">
-                                        <div class="vp-table--text">
-                                            {!! $item['text'] !!}
+                                        <div class="vp-table--col">
+                                            <div class="vp-table--text">
+                                                {!! $item['text'] !!}
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            @endforeach
-                        @endif
-                    </div>
-                @endforeach
-            </div>
-        @endif
-    </div>
+                            </div>
+                        @endforeach
+                    @endif
+                </div>
+            @endforeach
+        </div>
+    @endif
 </div>
