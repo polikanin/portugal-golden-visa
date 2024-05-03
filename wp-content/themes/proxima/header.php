eec68ec0eda145ce1be7880e97cbb2c0b392466a
Anatolii Mykhailov <r.link.club@gmail.com>
2024-05-03 21:36:51 +0300

    Add title and cookie style

--- wp-content/themes/proxima/header.php
+++ wp-content/themes/proxima/header.php
@@ -1,9 +1,16 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
+    <title><?php the_title(); ?></title>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <style>
+        [data-cky-tag='detail-powered-by'] {
+            display: none!important;
+        }
+    </style>
 
     <?php wp_head(); ?>
 </head>
